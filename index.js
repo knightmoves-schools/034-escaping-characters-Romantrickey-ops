4043225d6de96c580b37fb0b11e557ba8699f806
romantrickey-ops <roman.trickey@students.msswarriors.org>
2026-04-21 14:26:53 -0500

    Update index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+let introduction = 'dear Brenda,/n'+
+  it was very /"nice of you/" show up at the party./n" +
+  i hope you injoyed the party and have fun and hope to see you again /'hopefully/'./n/n' +
+  'sincerely,/n' +
+  'travis scott ://'
+document.getElementById("result").innerHTML = introduction;
