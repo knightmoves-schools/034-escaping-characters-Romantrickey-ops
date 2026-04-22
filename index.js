82d0565096f44253d20882dd586d9dc9485c0171
romantrickey-ops <roman.trickey@students.msswarriors.org>
2026-04-22 12:59:48 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var introDuction = 'dear Brenda,/n'+
   it was very nice of you to put "/introduction in the beginning of your sentence it was a /"nice touch/" to a start./n" 
+"i will cherish it /"
   'sincerely
-  'travis scott ://'
+  'travis scott :/'
 document.getElementById("result").innerHTML = introduction;
